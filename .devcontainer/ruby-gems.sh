a9f213120c6afbd747d79c798dc5bbddfae5aa0f
aetos <aetos382@users.noreply.github.com>
2026-04-17 22:43:36 +0900

    c

--- .devcontainer/ruby-gems.sh
+++
@@ -1,5 +0,0 @@
-default_gem_path=$(/usr/local/ruby/bin/ruby -r rubygems -e 'puts Gem.default_path.join(":")')
-
-export GEM_HOME="${HOME}/.gem"
-export GEM_PATH="${GEM_HOME}:${default_gem_path}"
-export PATH="${GEM_HOME}/bin:${PATH}"
