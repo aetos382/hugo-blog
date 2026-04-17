dbde3dc0a60c5957b5e9b71bfcbced817a5cfbfd
aetos <aetos382@users.noreply.github.com>
2026-04-17 18:15:13 +0900

    fix

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,6 +1,3 @@
 curl -fsSL https://claude.ai/install.sh | bash
 
-bundle config set --global path "${HOME}/.bundle"
-bundle config set --global disable_shared_gems true
-
-(cd src && bundle install)
+(cd src && bundle install && bundle binstubs asciidoc)
