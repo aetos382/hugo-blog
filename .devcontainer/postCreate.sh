d55f0b723778fd23aad71133ae8c2824439c7d04
aetos <aetos382@users.noreply.github.com>
2026-04-18 05:48:07 +0900

    completions

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -7,3 +7,15 @@ bundle config set --global bin ${PWD}/src/bin
 (cd src && bundle install && bundle binstubs asciidoctor --force)
 
 curl -fqsLS https://claude.ai/install.sh | bash
+
+pwsh -NoProfile -NonInteractive -Command - <<'EOS'
+$profilePath = $profile.CurrentUserAllHosts
+$profileDirectory = $profilePath | Split-Path -Parent
+
+New-Item -ItemType Directory -Force -Path $profileDirectory > $null
+
+$completionPath = $profileDirectory | Join-Path -ChildPath hugo-completions.ps1
+
+hugo completion powershell > $completionPath
+". ${completionPath}" >> $profilePath
+EOS
