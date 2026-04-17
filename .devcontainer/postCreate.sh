426cca4bfffff36fb056b4bbaed0d7b0e9649537
aetos <aetos382@users.noreply.github.com>
2026-04-17 18:33:23 +0900

    do?

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,3 +1,7 @@
 curl -fsSL https://claude.ai/install.sh | bash
 
+bundle config set --global path ~/.bundle
+bundle config set --global disable_shared_gems true
+bundle config set --global bin ./bin
+
 (cd src && bundle install && bundle binstubs asciidoctor)
